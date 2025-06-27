72b2865fcbb10a8913cfdd86d1ab211d62aef7ea
petermedranol <ing.pedro.medrano@hotmail.com>
2025-06-27 11:31:37 -0500

    .env

--- frontend/src/environments/environmen.examplet.ts
+++ frontend/src/environments/environmen.examplet.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://localhost',
   recaptcha: {
     siteKey: 'your_recaptcha_site_key_here' // Reemplazar con clave de producción real
   }
